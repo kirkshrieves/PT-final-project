01ee9259716905ff495b5e28c51023f47643bafa
Kirk Shrieves <113265021+kirkshrieves@users.noreply.github.com>
2023-04-02 10:29:07 -0700

    Add files via upload

--- src/main/java/com/promineotech/mercedes/controller/MercedesDeleteController.java
+++ src/main/java/com/promineotech/mercedes/controller/MercedesDeleteController.java
@@ -1,12 +1,15 @@
 package com.promineotech.mercedes.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.promineotech.mercedes.entity.Chassis;
 
 public interface MercedesDeleteController {
-	@DeleteMapping
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/mercedes")
 	public static void deleteMercedes(@PathVariable String id) {
 		Chassis.delete(id);
 	}
